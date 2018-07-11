a6cd709b58996b5d1a7c2d998c9b339ae6161e51
avry <altan@ualberta.ca>
2018-07-10 22:45:49 -0600

    wrote components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const App = (props) => {
+	return (
+		<h2 className="text-center">
+			{props.headerMessage}
+		</h2>
+	);
+};
+
+
+//this validates the arguement(prop) type. Will show 
+//warning otherwise
+//.isRequired makes it so that if this component is called
+//without this prop, a warning will show
+App.propTypes = {
+	headerMessage: React.PropTypes.string
+};
+
+//this sets the default values of props
+App.defaultProps = {
+	headerMessage: 'Hello!!'
+}
+
+
 ReactDOM.render(
-	React.createElement('h2', null, 'Hello React'),
+	<App />,
 	document.getElementById('root')
 );
