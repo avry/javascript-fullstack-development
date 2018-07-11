bdbce6f584b7ea4802e6931837a8450e0bc460fe
avry <altan@ualberta.ca>
2018-07-10 21:32:30 -0600

    fixed the view.engine buggit status

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import apiRouter from './api';
 const server = express();
 
 //setting up ejs to work with express
-server.set('view.engine', 'ejs'); 
+server.set('view engine', 'ejs'); 
 
 
 server.get('/', (req, res) => {
@@ -15,18 +15,19 @@ server.get('/', (req, res) => {
 	}); //.render used for ejs files. .ejs extension not req	
 });
 
+server.use('/api', apiRouter); 
+
+
 // server.get('/about.html', (req, res) => {
 // 	fs.readFile('./about.html', (err, data) => {
 // 		res.send(data.toString());
 // 	});
 // });
-
 //the above lines can be replaced with
 server.use(express.static('public'));
 //.use is how we put a middleware in the express middleware stack
 // public is where we want our static assets to be hosted ON the file sys
 
-server.use('/api', apiRouter); 
 
 server.listen(config.port, () => {
 	console.info("express listening on port ", config.port);
