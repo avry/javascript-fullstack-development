605ff955910bde085b141e2183ec5cb2e9934f91
avry <altan@ualberta.ca>
2018-07-12 23:28:16 -0600

    created the ContestPreview component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
 import Header from './Header';
 import React from 'react';
+import ContestPreview from './ContestPreview';
 
 
 class App extends React.Component {
 	state = {
 		pageHeader: 'Naming Contests'
 	};
+	componentDidMount() {
+		//timers listeners
+	}
+	componentWillUnmount() {
+		//clean timers, listeners
+	}
 	render() {
 		return (
 			<div className="App">
 				<Header message={this.state.pageHeader} />
 				<div>
-					-----
+					{this.props.contests.map(contest =>
+						<ContestPreview {...contest} />
+					)}
 				</div>
 			</div>
 		);
