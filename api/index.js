6d86d194f950875d33feb58fabc59a2c5c57054d
avry <altan@ualberta.ca>
2018-08-24 01:00:57 -0600

    uploaded sample data to a mongodb and rewrote the api to read from MongoDB

--- api/index.js
+++ api/index.js
@@ -1,24 +1,44 @@
 import express from 'express';
-import data from '../src/testData';
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+import config from '../config';
+
+
+let mdb;
+MongoClient.connect(config.mongodbUri, (err, db) => {
+	assert.equal(null, err);
+
+	mdb = db;
+});
 
 const router = express.Router();
-const contests = data.contests.reduce((obj, contest) => {
-	obj[contest.id] = contest;
-	return obj;
-}, {});
+
 
 router.get('/contests', (req, res) => {
-	res.send({
-		contests: contests
-	});
+	let contests = {};
+	mdb.collection('contests').find({})
+		.project({
+			id:1,
+			categoryName: 1,
+			contestName: 1,
+		})
+		.each((err, contest) => {
+			assert.equal(null, err);
+
+			if (!contest) {//no more contests to process
+				res.send({ contests });
+				return;
+			}
+			contests[contest.id] = contest;
+		});
 }); 
 
 router.get('/contests/:contestId', (req, res) => {
 	//the id will thus be available as req.params.contestId
-	let contest = contests[req.params.contestId];
-	contest.description = 'some placeholder description';
-
-	res.send(contest);
+	mdb.collection('contests')
+		.findOne({ id: Number(req.params.contestId) })
+		.then(contest => res.send(contest))
+		.catch(console.error);
 }); 
 
 export default router;
